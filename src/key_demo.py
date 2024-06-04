5797924b3b99116f2e7afbcbb4e7bd790a1d1adb
John Williamson <johnhw@gmail.com>
2024-06-04 19:01:15 +0100

    more complete pf sections

--- src/key_demo.py
+++ src/key_demo.py
@@ -2,6 +2,7 @@ from key_test import capture_keys
 from tkanvas import TKanvas
 from key_model import KeyModel
 import cmasher
+import csv
 import queue
 import shelve
 from tkinter import mainloop
@@ -42,6 +43,16 @@ class TKMatrix(object):
                 )
                 self.rects.append(rect)
 
+    def normalised_coordinates(self, x, y):
+        # return coordinates in range [0,1] from an input
+        # location in screen coordinates
+        width = self.shape[1] * self.size
+        height = self.shape[0] * self.size
+        nx, ny = (x - self.origin[0]) / width, (y - self.origin[1]) / height
+        
+        return nx, ny 
+    
+
     def update(self, matrix):
         assert matrix.shape == self.shape
         for i in range(self.shape[0]):
@@ -49,6 +60,27 @@ class TKMatrix(object):
                 ix = i * self.shape[1] + j
                 color = self.cmap(matrix[i, j])[:3]
                 self.canvas.canvas.itemconfig(self.rects[ix], fill=tkcolor(color))
+
+class KeyRecorder:
+    def __init__(self, fname, overwrite=False):
+        if fname is not None:
+            if overwrite:
+                self.record_file = open(fname, "w")
+            else:
+                self.record_file = open(fname, "a")
+            self.record_csv = csv.writer(self.record_file)
+        else:
+            self.record_file = None # dummy writer
+
+    def write_row(self, inp, out):
+        inp = np.array(inp).astype(np.float32).flatten().tolist()
+        out = np.array(out).astype(np.float32).flatten().tolist()
+        if self.record_file is not None:
+            self.record_csv.writerow(inp + out)
+
+    def close(self):
+        if self.record_file is not None:
+            self.record_file.close()
 
 
 
@@ -62,6 +94,8 @@ class KeyDisplay(object):
         bw=0.03,   
         intensity=1.8,
         slc=(9,30),
+        record_fname=None,
+        overwrite=False
     ):        
         
         self.q = q  # keyboard input
@@ -71,10 +105,15 @@ class KeyDisplay(object):
         self.block_size = 24
         self.slc = slc
         self.status = "OK"
+        self.cursor_radius = 10
+        
+        # for recording data to a file
+        self.key_recorder = KeyRecorder(fname=record_fname, overwrite=overwrite)                
                 
         self.canvas = TKanvas(
             draw_fn=self.draw,
             tick_fn=self.tick,
+            event_fn=self.event,
             w=self.block_size * (slc[1]+1),
             h=self.block_size * (slc[0]+2),
         )
@@ -87,13 +126,28 @@ class KeyDisplay(object):
             text=str(self.status),
             fill="white",
             anchor="w",
-            font=("Arial", 16),
+            font=("Arial", 16),            
         )
 
+        self.cursor_point = self.canvas.circle(-100, -100, self.cursor_radius, fill="red")
 
+    def event(self, src, etype, event):        
+        if etype == "mouseup":
+            # on click, set the cursor display
+            click = [event.x, event.y]
+            self.canvas.canvas.moveto(self.cursor_point, click[0]-self.cursor_radius, click[1]-self.cursor_radius)
+            key_slice = self.model.key_buffer[:self.slc[0], :self.slc[1]]
+            nx, ny = self.matrix_display.normalised_coordinates(*click) # normalise screen coords to [0,1]
+            
+            self.key_recorder.write_row(key_slice, [nx, ny])
+        
+            
     def tick(self, dt):
         try:
             while not self.q.empty():
+                # clear cursor
+                self.canvas.canvas.moveto(self.cursor_point, -100,-100)
+                # read results
                 result = self.q.get(block=False)
                 if result:
                     arr_bytes, t, code, event, name  = result                
@@ -104,6 +158,7 @@ class KeyDisplay(object):
                         self.model.up(code)
                     
                 else:
+                    self.key_recorder.close()
                     self.canvas.quit(None)
                    
         except queue.Empty:
@@ -129,6 +184,13 @@ def key_tk(*args, **kwargs):
     keyboard.restore_state(current_state)
     return current_state
 
+import click 
+@click.command()
+@click.option("--file", default=None, help="Record filename")
+@click.option("--overwrite", is_flag=True, help="Overwrite record file")
+def start_key_display(file=None, overwrite=False):
+    k = KeyDisplay(q, record_fname=file, overwrite=overwrite)
+    mainloop()
 
 if __name__ == "__main__":
     import keyboard, atexit
@@ -137,7 +199,7 @@ if __name__ == "__main__":
     atexit.register(keyboard.restore_state, current_state)
     q = Queue()
     keys = Process(target=capture_keys, args=(q,))
-    keys.start()
-    k = KeyDisplay(q)
-    mainloop()
+    keys.start()    
+    start_key_display()
+    
 
--- src/key_model.py
+++ src/key_model.py
@@ -18,26 +18,26 @@ class KeyModel:
         self.bw = bw 
         self.intensity = intensity
         
-    def kernel(self, ix, iy):
+    def kernel(self, ix, iy, bw, intensity):
         # convert to (0,1) normalised coordinates
         x = (ix) * self.x_scale #+ self.x_offset
         y = (iy) * self.y_scale #+ self.y_offset        
         mx, my = self.key_mesh 
         # eval kernel
-        return np.exp(-((((mx-x) ** 2 + (my-y) ** 2) / (2 * self.bw ** 2)))) * self.intensity
+        return np.exp(-((((mx-x) ** 2 + (my-y) ** 2) / (2 * bw ** 2)))) * intensity
 
     def up(self, code):
         if code not in self.states:
             self.states[code] = "down"
         if code in self.key_map and self.states[code] == "down":
-            self.key_image -= self.kernel(*self.key_map[code])
+            self.key_image -= self.kernel(*self.key_map[code], self.bw, self.intensity)
             self.states[code] = "up"
 
     def down(self, code):
         if code not in self.states:
             self.states[code] = "up"
         if code in self.key_map and self.states[code] == "up":
-            self.key_image += self.kernel(*self.key_map[code])
+            self.key_image += self.kernel(*self.key_map[code], self.bw, self.intensity)
             self.states[code] = "down"
 
     def tick(self):
@@ -45,3 +45,25 @@ class KeyModel:
         self.key_buffer += np.random.normal(0, self.noise, self.key_image.shape)
         self.key_buffer = np.clip(self.key_buffer, 0, 1)        
 
+    def simulate_press(self, x, y, size=0.02, intensity=1.0, steps=5, slc=(9, 30)):
+        # simulate a key press at x,y
+        self.key_image[:] = 0.0
+        self.key_image += self.kernel(x, y, bw=size, intensity=intensity)
+        for i in range(steps):
+            self.tick()
+        return self.key_buffer
+
+
+class KeySimulator:
+    def __init__(self, size, slc, **kwargs):
+        key_map = {"width":1.0, "height":slc[0]/slc[1]}
+        self.km = KeyModel(key_map, size, **kwargs)
+        self.size = size
+        self.slc = slc
+
+    def press(self, x, y, size=0.04, intensity=1.5, steps=5, slc=(9, 30)):
+        x = self.slc[1] * x / self.size
+        y = self.slc[0] * y / self.size
+        img = self.km.simulate_press(x, y, size, intensity, steps)
+        
+        return img[:slc[0], :slc[1]]
